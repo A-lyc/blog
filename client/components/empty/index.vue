e007bda373d13f6d8636fc1152cc993e046ce884
q-jason <596908546@qq.com>
2019-06-01 15:41:46 +0800

    待解决：update 事件不触发

---
+++ client/components/empty/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    class="empty"
+    src="./images/empty.png">
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .empty {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 100%;
+  }
+</style>
